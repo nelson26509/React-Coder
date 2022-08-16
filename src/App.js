5f58c3b215c6c13c5c53e432f88269e56e130f57
nelsonbotello <nelsonbotello110@hotmail.com>
2022-08-16 19:41:58 -0300

    subiendo app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Curso Coder House React</h1>
+        <h2>Alumno:Nelson Botello</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
