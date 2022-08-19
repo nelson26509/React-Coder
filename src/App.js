d9d57296d6da7b6988490a92fd102da55c212ada
nelsonbotello <nelsonbotello110@hotmail.com>
2022-08-18 22:57:56 -0300

    subiendo Nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar1 from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Curso Coder House React</h1>
-        <h2>Alumno:Nelson Botello</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar1></NavBar1>
       </header>
     </div>
   );
